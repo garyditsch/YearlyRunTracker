0567a17f63846271918d95598fa2a60d231824b8
Gary Ditsch <garyditsch@gmail.com>
2022-05-14 14:00:16 -0400

    updates to start adding year to date comparison bars for mileage totals

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+import { isThisYear } from "date-fns";
 import { ru } from "date-fns/locale";
 import { calendar } from "./modules/calendar.js"
 import { theData, runTable, runTable2 } from './modules/data'
@@ -6,12 +7,13 @@ const calendar_svg = d3.select("#calendar_svg_2022")
 const comparison_cal = d3.select("#comparison_year")
 let dateControl = document.querySelector('select');
 
-const totalToDate = (myData) => {
+const totalToDate = (myData, varYear) => {
     console.log(myData)
     const total = myData.reduce((acc, curr) => {
         //get day of year for today
         let today = new Date()
-        let year = today.getFullYear()
+        let year = varYear
+        console.log('the year that matters', year)
         let start = new Date(today.getFullYear(), 0, 0)
         let diff = (today - start) + ((start.getTimezoneOffset() - today.getTimezoneOffset()) * 60 * 100)
         let oneDay = 1000 * 60 * 60 * 24
@@ -26,19 +28,19 @@ const totalToDate = (myData) => {
     return total
 }
 
-const getYearlyTotal = async () => {
+const getYearlyTotal = async (year) => {
     if(localStorage.getItem('runData')){
-    const myData = JSON.parse(localStorage.getItem('runData'))
-    console.log(myData)
-    const total = totalToDate(myData)
-    console.log(total.toFixed(0))
+        const myData = JSON.parse(localStorage.getItem('runData'))
+        const total = await totalToDate(myData, year)
+        return total.toFixed(0)
     } else {
         console.log('sorry no data')
     }
     
 }
 
-const thisData = getYearlyTotal()
+const thisData = getYearlyTotal('2017')
+console.log('theeeeeeeee data', thisData)
 
 
 const drawYearlyCalender = async () => {
@@ -60,6 +62,49 @@ const drawYearlyCalender = async () => {
             break;
         case '2021':
             console.log('first')
+            const progressBars = document.getElementById('progress-bars')
+            const labelDiv = document.createElement('div')
+            const label = document.createElement('h3')
+            label.innerHTML = `How many miles have I run on this day in each year?`
+            labelDiv.append(label)
+            progressBars.append(labelDiv)
+
+            getYearlyTotal('2022')
+                .then(response => {
+                    console.log(response)
+                    const progressBars = document.getElementById('progress-bars')
+                    console.log(progressBars)
+                    const progressBarsDiv = document.createElement('div')
+                    progressBarsDiv.className = 'progressDiv1'
+                    progressBars.append(progressBarsDiv)
+                    const title = document.createElement('h3')
+                    title.innerText = `2022, ${response} Miles`
+                    progressBarsDiv.append(title)
+                    const thisYearBar = document.createElement('progress')
+                    thisYearBar.value = response
+                    thisYearBar.max = 2000
+                    thisYearBar.className = 'progressBar1'
+                    progressBarsDiv.append(thisYearBar)
+                }
+            )
+            getYearlyTotal('2021')
+                .then(response => {
+                    console.log(response)
+                    const progressBars = document.getElementById('progress-bars')
+                    console.log(progressBars)
+                    const progressBarsDiv = document.createElement('div')
+                    progressBarsDiv.className = 'progressDiv2'
+                    progressBars.append(progressBarsDiv)
+                    const title = document.createElement('h3')
+                    title.innerText = `2021, ${response} Miles`
+                    progressBarsDiv.append(title)
+                    const thisYearBar = document.createElement('progress')
+                    thisYearBar.value = response
+                    thisYearBar.max = 2000
+                    thisYearBar.className = 'progressBar2'
+                    progressBarsDiv.append(thisYearBar)
+                }
+            )
             calendar(theData, runTable, runTable2, calendar_svg, {
                 'startDate': '12/31/2021',
                 'endDate': '1/1/2023',
